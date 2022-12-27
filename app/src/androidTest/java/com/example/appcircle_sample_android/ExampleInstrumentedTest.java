3e066cedcd63fa467bbc58e79e5fab096a18432c
Mustafa Dur <971530+tosbaha@users.noreply.github.com>
2022-12-27 17:35:32 +0300

    Update package name for working tests

--- app/src/androidTest/java/com/example/appcircle_sample_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcircle_sample_android/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.appcircle_sample_android", appContext.getPackageName());
+        assertEquals("com.appcircle.appcircle_sample_android", appContext.getPackageName());
     }
 }
