131ccc4a6db658f61e194ebd3d0598d4f5c57531
Halit Aksoy <jhalitaksoy@gmail.com>
2023-11-13 15:48:08 +0300

    upgrade espresso version

--- app/src/androidTest/java/com/example/appcircle_sample_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcircle_sample_android/ExampleInstrumentedTest.java
@@ -3,9 +3,7 @@ package com.example.appcircle_sample_android;
 import android.content.Context;
 
 import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.ViewAction;
-import androidx.test.runner.AndroidJUnit4;
-
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
