15241d8bb4c22aaf632143a8bc996c488c0a9f45
Guven Karanfil <guven.karanfil@smartface.io>
2024-09-03 15:53:25 +0300

    update download url

--- app/src/main/java/com/example/appcircle_sample_android/Environment.java
+++ app/src/main/java/com/example/appcircle_sample_android/Environment.java
@@ -1,7 +1,6 @@
 package com.example.appcircle_sample_android;
 
 public class Environment {
-    static String STORE_PREFIX = "";
     static String STORE_URL = "";
     static String ORGANIZATION_ID = "";
     static String SECRET = "";
