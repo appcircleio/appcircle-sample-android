3b552ba0dac13142a94e3a01d1fc0f9afb16367a
doruk batur <dbatur4@hotmail.com>
2025-05-15 13:46:21 +0300

    url test

--- app/src/test/java/com/example/appcircle_sample_android/URLValidatorUnitTest.java
+++ app/src/test/java/com/example/appcircle_sample_android/URLValidatorUnitTest.java
@@ -25,7 +25,7 @@ public class URLValidatorUnitTest {
     @Test
     public void invalid_url_test() {
         boolean isValid = URLValidator.isValid("htps://www.google.com");
-        assertTrue(isValid);
+        assertFalse(isValid);
     }
 
     @Test
